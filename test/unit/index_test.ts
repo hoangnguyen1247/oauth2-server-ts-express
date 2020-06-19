87061b0f52f3c48ba8b9cf22aa2a8b09322fd760
Hoang <hoangnguyen1247@gmail.com>
2020-06-19 23:35:35 +0700

    bump version

--- test/unit/index_test.ts
+++ test/unit/index_test.ts
@@ -13,191 +13,191 @@ import ExpressOAuthServer from "../..";
  * Test `ExpressOAuthServer`.
  */
 
-describe('ExpressOAuthServer', function() {
-  var app;
+describe('ExpressOAuthServer', function () {
+    var app;
 
-  beforeEach(function() {
-    app = express();
-  });
+    beforeEach(function () {
+        app = express();
+    });
 
-  describe('authenticate()', function() {
-    it('should call `authenticate()`', function(done) {
-      var oauth = new ExpressOAuthServer({ model: {} });
+    describe('authenticate()', function () {
+        it('should call `authenticate()`', function (done) {
+            var oauth = new ExpressOAuthServer({ model: {} });
 
-      sinon.stub(oauth.server, 'authenticate').returns({});
+            sinon.stub(oauth.server, 'authenticate').returns({});
 
-      app.use(oauth.authenticate());
+            app.use(oauth.authenticate());
 
-      request(app.listen())
-        .get('/')
-        .end(function() {
-          oauth.server.authenticate.callCount.should.equal(1);
-          oauth.server.authenticate.firstCall.args.should.have.length(3);
-          oauth.server.authenticate.firstCall.args[0].should.be.an.instanceOf(Request);
-          oauth.server.authenticate.firstCall.args[1].should.be.an.instanceOf(Response);
-          should.not.exist(oauth.server.authenticate.firstCall.args[2])
-          oauth.server.authenticate.restore();
+            request(app.listen())
+                .get('/')
+                .end(function () {
+                    oauth.server.authenticate.callCount.should.equal(1);
+                    oauth.server.authenticate.firstCall.args.should.have.length(3);
+                    oauth.server.authenticate.firstCall.args[0].should.be.an.instanceOf(Request);
+                    oauth.server.authenticate.firstCall.args[1].should.be.an.instanceOf(Response);
+                    should.not.exist(oauth.server.authenticate.firstCall.args[2])
+                    oauth.server.authenticate.restore();
 
-          done();
+                    done();
+                });
+        });
+
+        it('should call `authenticate()` with options', function (done) {
+            var oauth = new ExpressOAuthServer({ model: {} });
+
+            sinon.stub(oauth.server, 'authenticate').returns({});
+
+            app.use(oauth.authenticate({ options: true }));
+
+            request(app.listen())
+                .get('/')
+                .end(function () {
+                    oauth.server.authenticate.callCount.should.equal(1);
+                    oauth.server.authenticate.firstCall.args.should.have.length(3);
+                    oauth.server.authenticate.firstCall.args[0].should.be.an.instanceOf(Request);
+                    oauth.server.authenticate.firstCall.args[1].should.be.an.instanceOf(Response);
+                    oauth.server.authenticate.firstCall.args[2].should.eql({ options: true });
+                    oauth.server.authenticate.restore();
+                    done();
+                });
         });
     });
 
-    it('should call `authenticate()` with options', function(done) {
-      var oauth = new ExpressOAuthServer({ model: {} });
+    describe('authorize()', function () {
+        it('should call `authorize()` and end middleware execution', function (done) {
+            var nextMiddleware = sinon.spy()
+            var oauth = new ExpressOAuthServer({ model: {} });
 
-      sinon.stub(oauth.server, 'authenticate').returns({});
+            sinon.stub(oauth.server, 'authorize').returns({});
 
-      app.use(oauth.authenticate({options: true}));
+            app.use(oauth.authorize());
+            app.use(nextMiddleware);
 
-      request(app.listen())
-        .get('/')
-        .end(function() {
-          oauth.server.authenticate.callCount.should.equal(1);
-          oauth.server.authenticate.firstCall.args.should.have.length(3);
-          oauth.server.authenticate.firstCall.args[0].should.be.an.instanceOf(Request);
-          oauth.server.authenticate.firstCall.args[1].should.be.an.instanceOf(Response);
-          oauth.server.authenticate.firstCall.args[2].should.eql({options: true});
-          oauth.server.authenticate.restore();
-          done();
+            request(app.listen())
+                .get('/')
+                .end(function () {
+                    oauth.server.authorize.callCount.should.equal(1);
+                    oauth.server.authorize.firstCall.args.should.have.length(3);
+                    oauth.server.authorize.firstCall.args[0].should.be.an.instanceOf(Request);
+                    oauth.server.authorize.firstCall.args[1].should.be.an.instanceOf(Response);
+                    should.not.exist(oauth.server.authorize.firstCall.args[2]);
+                    oauth.server.authorize.restore();
+                    nextMiddleware.called.should.be.false();
+                    done();
+                });
         });
-    });
-  });
 
-  describe('authorize()', function() {
-    it('should call `authorize()` and end middleware execution', function(done) {
-      var nextMiddleware = sinon.spy()
-      var oauth = new ExpressOAuthServer({ model: {} });
+        it('should call `authorize()` and continue middleware chain', function (done) {
+            var nextMiddleware = sinon.spy()
+            var oauth = new ExpressOAuthServer({ model: {}, continueMiddleware: true });
 
-      sinon.stub(oauth.server, 'authorize').returns({});
+            sinon.stub(oauth.server, 'authorize').returns({});
 
-      app.use(oauth.authorize());
-      app.use(nextMiddleware);
+            app.use(oauth.authorize());
+            app.use(nextMiddleware);
 
-      request(app.listen())
-        .get('/')
-        .end(function() {
-          oauth.server.authorize.callCount.should.equal(1);
-          oauth.server.authorize.firstCall.args.should.have.length(3);
-          oauth.server.authorize.firstCall.args[0].should.be.an.instanceOf(Request);
-          oauth.server.authorize.firstCall.args[1].should.be.an.instanceOf(Response);
-          should.not.exist(oauth.server.authorize.firstCall.args[2]);
-          oauth.server.authorize.restore();
-          nextMiddleware.called.should.be.false();
-          done();
+            request(app.listen())
+                .get('/')
+                .end(function () {
+                    oauth.server.authorize.callCount.should.equal(1);
+                    oauth.server.authorize.firstCall.args.should.have.length(3);
+                    oauth.server.authorize.firstCall.args[0].should.be.an.instanceOf(Request);
+                    oauth.server.authorize.firstCall.args[1].should.be.an.instanceOf(Response);
+                    should.not.exist(oauth.server.authorize.firstCall.args[2]);
+                    oauth.server.authorize.restore();
+                    nextMiddleware.called.should.be.true();
+                    nextMiddleware.args[0].length.should.eql(3);
+                    done();
+                });
         });
-    });
 
-    it('should call `authorize()` and continue middleware chain', function(done) {
-      var nextMiddleware = sinon.spy()
-      var oauth = new ExpressOAuthServer({ model: {}, continueMiddleware: true });
+        it('should call `authorize()` with options', function (done) {
+            var oauth = new ExpressOAuthServer({ model: {} });
 
-      sinon.stub(oauth.server, 'authorize').returns({});
+            sinon.stub(oauth.server, 'authorize').returns({});
 
-      app.use(oauth.authorize());
-      app.use(nextMiddleware);
+            app.use(oauth.authorize({ options: true }));
 
-      request(app.listen())
-        .get('/')
-        .end(function() {
-          oauth.server.authorize.callCount.should.equal(1);
-          oauth.server.authorize.firstCall.args.should.have.length(3);
-          oauth.server.authorize.firstCall.args[0].should.be.an.instanceOf(Request);
-          oauth.server.authorize.firstCall.args[1].should.be.an.instanceOf(Response);
-          should.not.exist(oauth.server.authorize.firstCall.args[2]);
-          oauth.server.authorize.restore();
-          nextMiddleware.called.should.be.true();
-          nextMiddleware.args[0].length.should.eql(3);
-          done();
-        });
-    });
-
-    it('should call `authorize()` with options', function(done) {
-      var oauth = new ExpressOAuthServer({ model: {} });
-
-      sinon.stub(oauth.server, 'authorize').returns({});
-
-      app.use(oauth.authorize({options: true}));
-
-      request(app.listen())
-        .get('/')
-        .end(function() {
-          oauth.server.authorize.callCount.should.equal(1);
-          oauth.server.authorize.firstCall.args.should.have.length(3);
-          oauth.server.authorize.firstCall.args[0].should.be.an.instanceOf(Request);
-          oauth.server.authorize.firstCall.args[1].should.be.an.instanceOf(Response);
-          oauth.server.authorize.firstCall.args[2].should.eql({options: true});
-          oauth.server.authorize.restore();
-          done();
-        });
-    });
-  });
-
-  describe('token()', function() {
-    it('should call `token()` and end middleware chain', function(done) {
-      var nextMiddleware = sinon.spy()
-      var oauth = new ExpressOAuthServer({ model: {} });
-
-      sinon.stub(oauth.server, 'token').returns({});
-
-      app.use(oauth.token());
-      app.use(nextMiddleware);
-
-      request(app.listen())
-        .get('/')
-        .end(function() {
-          oauth.server.token.callCount.should.equal(1);
-          oauth.server.token.firstCall.args.should.have.length(3);
-          oauth.server.token.firstCall.args[0].should.be.an.instanceOf(Request);
-          oauth.server.token.firstCall.args[1].should.be.an.instanceOf(Response);
-          should.not.exist(oauth.server.token.firstCall.args[2]);
-          oauth.server.token.restore();
-          nextMiddleware.called.should.be.false();
-          done();
+            request(app.listen())
+                .get('/')
+                .end(function () {
+                    oauth.server.authorize.callCount.should.equal(1);
+                    oauth.server.authorize.firstCall.args.should.have.length(3);
+                    oauth.server.authorize.firstCall.args[0].should.be.an.instanceOf(Request);
+                    oauth.server.authorize.firstCall.args[1].should.be.an.instanceOf(Response);
+                    oauth.server.authorize.firstCall.args[2].should.eql({ options: true });
+                    oauth.server.authorize.restore();
+                    done();
+                });
         });
     });
 
-    it('should call `token()` and continue middleware chain', function(done) {
-      var nextMiddleware = sinon.spy()
-      var oauth = new ExpressOAuthServer({ model: {}, continueMiddleware: true });
+    describe('token()', function () {
+        it('should call `token()` and end middleware chain', function (done) {
+            var nextMiddleware = sinon.spy()
+            var oauth = new ExpressOAuthServer({ model: {} });
 
-      sinon.stub(oauth.server, 'token').returns({});
+            sinon.stub(oauth.server, 'token').returns({});
 
-      app.use(oauth.token());
-      app.use(nextMiddleware);
+            app.use(oauth.token());
+            app.use(nextMiddleware);
 
-      request(app.listen())
-        .get('/')
-        .end(function() {
-          oauth.server.token.callCount.should.equal(1);
-          oauth.server.token.firstCall.args.should.have.length(3);
-          oauth.server.token.firstCall.args[0].should.be.an.instanceOf(Request);
-          oauth.server.token.firstCall.args[1].should.be.an.instanceOf(Response);
-          should.not.exist(oauth.server.token.firstCall.args[2]);
-          oauth.server.token.restore();
-          nextMiddleware.called.should.be.true();
-          nextMiddleware.args[0].length.should.eql(3);
-          done();
+            request(app.listen())
+                .get('/')
+                .end(function () {
+                    oauth.server.token.callCount.should.equal(1);
+                    oauth.server.token.firstCall.args.should.have.length(3);
+                    oauth.server.token.firstCall.args[0].should.be.an.instanceOf(Request);
+                    oauth.server.token.firstCall.args[1].should.be.an.instanceOf(Response);
+                    should.not.exist(oauth.server.token.firstCall.args[2]);
+                    oauth.server.token.restore();
+                    nextMiddleware.called.should.be.false();
+                    done();
+                });
+        });
+
+        it('should call `token()` and continue middleware chain', function (done) {
+            var nextMiddleware = sinon.spy()
+            var oauth = new ExpressOAuthServer({ model: {}, continueMiddleware: true });
+
+            sinon.stub(oauth.server, 'token').returns({});
+
+            app.use(oauth.token());
+            app.use(nextMiddleware);
+
+            request(app.listen())
+                .get('/')
+                .end(function () {
+                    oauth.server.token.callCount.should.equal(1);
+                    oauth.server.token.firstCall.args.should.have.length(3);
+                    oauth.server.token.firstCall.args[0].should.be.an.instanceOf(Request);
+                    oauth.server.token.firstCall.args[1].should.be.an.instanceOf(Response);
+                    should.not.exist(oauth.server.token.firstCall.args[2]);
+                    oauth.server.token.restore();
+                    nextMiddleware.called.should.be.true();
+                    nextMiddleware.args[0].length.should.eql(3);
+                    done();
+                });
+        });
+
+        it('should call `token()` with options', function (done) {
+            var oauth = new ExpressOAuthServer({ model: {} });
+
+            sinon.stub(oauth.server, 'token').returns({});
+
+            app.use(oauth.token({ options: true }));
+
+            request(app.listen())
+                .get('/')
+                .end(function () {
+                    oauth.server.token.callCount.should.equal(1);
+                    oauth.server.token.firstCall.args.should.have.length(3);
+                    oauth.server.token.firstCall.args[0].should.be.an.instanceOf(Request);
+                    oauth.server.token.firstCall.args[1].should.be.an.instanceOf(Response);
+                    oauth.server.token.firstCall.args[2].should.eql({ options: true });
+                    oauth.server.token.restore();
+                    done();
+                });
         });
     });
-
-    it('should call `token()` with options', function(done) {
-      var oauth = new ExpressOAuthServer({ model: {} });
-
-      sinon.stub(oauth.server, 'token').returns({});
-
-      app.use(oauth.token({options: true}));
-
-      request(app.listen())
-        .get('/')
-        .end(function() {
-          oauth.server.token.callCount.should.equal(1);
-          oauth.server.token.firstCall.args.should.have.length(3);
-          oauth.server.token.firstCall.args[0].should.be.an.instanceOf(Request);
-          oauth.server.token.firstCall.args[1].should.be.an.instanceOf(Response);
-          oauth.server.token.firstCall.args[2].should.eql({options: true});
-          oauth.server.token.restore();
-          done();
-        });
-    });
-  });
 });
